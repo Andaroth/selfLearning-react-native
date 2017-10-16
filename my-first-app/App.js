e088bb61dc8aa5874271897fa049ab77d06b8e54
Anda <ax.fiolle@gmail.com>
2017-10-16 11:51:21 +0200

    J'arrive même pas à utiliser un asset image local è___é

--- my-first-app/App.js
+++ my-first-app/App.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, Image } from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Image source={{ uri: 'file///assets/img/pepe.jpg'}} style={{width:193,height:110}}/>
+        <Text style={{color: 'red'}}>Espèce de pauvre type, tu fais du React au lieu de bosser ton JavaScript !</Text>
       </View>
     );
   }
@@ -16,7 +15,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',  
     alignItems: 'center',
     justifyContent: 'center',
   },
